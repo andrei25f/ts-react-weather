fc91d56d7ae8d5b1e2a14ed19d7945a25de0067e
Andrey <andrey25f@gmail.com>
2024-09-21 21:40:22 +0200

    add thunk with Props

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,34 +1,15 @@
 import { FormEvent, useState } from 'react'
 import { useAppDispatch } from '../app/hooks';
-import { putWeatherInfo } from '../features/slices/weatherSlice';
-import { putMessage } from '../features/slices/messageSlice';
-import { api_key, base_url } from '../utils/constant';
+import { fetchWeatherByCity } from '../features/slices/weatherSlice';
 
 function Form() {
     const [city, setCity] = useState('');
     const dispatch = useAppDispatch();
 
-    const getCity = (e: FormEvent) => {
+    const getCity = async (e: FormEvent) => {
         e.preventDefault();
-        getWeather(city);
+        await dispatch(fetchWeatherByCity({dispatch, city}));
         setCity('');
-    }
-
-    const getWeather = async (city: string) => {
-        try {
-            const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
-            const data = await response.json();
-            dispatch(putWeatherInfo({
-                city: data.name,
-                country: data.sys.country,
-                temp: data.main.temp,
-                pressure: data.main.pressure,
-                sunset: data.sys.sunset
-            }))
-            dispatch(putMessage(''))
-        } catch (e) {
-            dispatch(putMessage('Enter correct city name'))
-        }
     }
 
     return (
--- src/features/slices/weatherSlice.ts
+++ src/features/slices/weatherSlice.ts
@@ -1,14 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { WeatherData } from "../../utils/constant";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { api_key, base_url, WeatherData } from "../../utils/constant";
+import { putMessage } from "./messageSlice";
+import { AppDispatch } from "../../app/store";
 
 const weatherSlice = createSlice({
     name: 'weather',
     initialState: {} as WeatherData,
-    reducers: {
-        putWeatherInfo: (_state, action) => action.payload
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchWeatherByCity.fulfilled, (_state, action) => action.payload)
     }
 })
 
-export const { putWeatherInfo } = weatherSlice.actions
+interface Props {
+    dispatch: AppDispatch,
+    city: string
+}
+
+export const fetchWeatherByCity = createAsyncThunk(
+    'weather/fetchWeatherByCity',
+    async ({ dispatch, city }: Props) => {
+        try {
+            const response = await fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`);
+            const data = await response.json();
+            dispatch(putMessage(''));
+            return {
+                city: data.name,
+                country: data.sys.country,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                sunset: data.sys.sunset
+            }
+        } catch {
+            dispatch(putMessage('Enter correct city name'))
+        }
+    }
+)
+
+export const {} = weatherSlice.actions
 
 export default weatherSlice.reducer
